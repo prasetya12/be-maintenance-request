ce8c30c97b5d6d46c144df7617c127eaaf7bfb8b
prasetya12 <rizalprasetya22@gmail.com>
2025-03-08 13:30:41 +0700

    feat:add cors

--- app.ts
+++ app.ts
@@ -1,15 +1,14 @@
 import { createHandler } from "graphql-http/lib/use/express";
 import express, { Express, Request, Response, Application } from 'express';
 import { schema } from "./src/graphql";
-
+import cors from "cors";
 import dotenv from 'dotenv';
 
-//For env File 
 dotenv.config();
 
+
 const app: Application = express();
-
-
+app.use(cors());
 app.use(
     "/graphql",
     createHandler({
